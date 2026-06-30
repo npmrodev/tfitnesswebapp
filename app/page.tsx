a126ac514208df117b3ad726794a4660e30d1c09
npmrodev <rodwinvicquerra@spup.edu.ph>
2026-07-01 07:28:51 +0800

    Simplify root page to always redirect to sign-in

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import { headers } from 'next/headers'
-import { auth } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
 export default async function Home() {
-  const session = await auth.api.getSession({ headers: await headers() })
-  
-  if (session?.user) {
-    redirect('/dashboard')
-  } else {
-    redirect('/sign-in')
-  }
+  redirect('/sign-in')
 }
